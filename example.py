77f09e12a82dd5a9563cb716c35572b497b955a8
Ivan Shivalov <142020129+intexcor@users.noreply.github.com>
2025-01-02 19:46:37 +0300

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-import torch
-import monotonic_align
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-v, m = (torch.randn(14, 636, 281, requires_grad=False, device=device), 
-        torch.randn(14, 636, 281, requires_grad=False, device=device))
-print(monotonic_align.maximum_path_cpp(v, m))

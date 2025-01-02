ef077afee4e3dfec05ad216278cdba0e1f5c9fe9
Ivan <ivansivalov396@gmail.com>
2025-01-03 01:59:58 +0300

    add pyi file

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import CppExtension, BuildExtension, include_paths
+from torch.utils.cpp_extension import CppExtension, BuildExtension
 
 
 ext_modules = [
       CppExtension(
             "monotonic_align",
             ["monotonic_align_cpp.cpp"],
-            include_dirs=include_paths(),
+
       )
 ]
 
 setup(
       name='monotonic_align',
-      version='0.0.17',
-      packages=find_packages(),
+      version='0.0.25',
       author='Ivan Shivalov',
+      packages=find_packages(),
       author_email='ivansivalov396@gmail.com',
       description='Module with search monotonic alignment algorithm',
       cmdclass={"build_ext": BuildExtension}, ext_modules=ext_modules,
+      extra_objects=["monotonic_align.pyi"]
 )
